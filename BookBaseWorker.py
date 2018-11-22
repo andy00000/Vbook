43512d3d0e1c646d2f0b003eb4170e6cc54d6bec
Andrey Shmyrov <andrey.shmyrov@gmail.com>
2018-11-22 08:30:59 +0300

    Initial commit

---
+++ BookBaseWorker.py
@@ -0,0 +1,18 @@
+"""
+BookBaseWorker - module for read/write data into DataBase.1 (DataBase of books)
+
+DataBase structure:
+
+1. Book name -> STRING
+2. Book author (authors) -> LIST OF STRINGS
+3. The year of publishing -> DATA
+4. ISBN -> STRING
+5. Format -> SET OF STRINGS
+6. Number of pages -> INTEGER
+7. Number of printed sheets -> INTEGER
+8. Binding -> SET OF STRINGS
+9. Publisher (publishing house) -> SET OF STRINGS
+10. Book series -> SET OF STRINGS
+
+"""
+
